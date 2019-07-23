0a76cd899d9ced3b14c01595465ac0cc6e6295fc
Mircea Ulinic <mulinic@digitalocean.com>
2019-07-23 14:09:43 +0100

    Relese a1

--- setup.py
+++ setup.py
@@ -7,15 +7,22 @@ with open("requirements.txt", "r") as fs:
     reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
 
 setup(
-    name="napalm-mos",
-    version="2.4.1",
-    packages=find_packages(),
+    name="napalm-iosxr-grpc",
+    version="0.0.1a1",
+    packages=find_packages(exclude=("test*",)),
     author="Mircea Ulinic",
     author_email="ping@mirceaulinic.net",
     description="Network Automation and Programmability Abstraction Layer with Multivendor support",
     classifiers=[
         "Topic :: Utilities",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
     ],
